3d1d202e4c93303ef2f493d14c6a30da9c41a52d
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-06-19 13:24:23 +0530

    Day-1 Session-2

--- Welcome.java
+++ Welcome.java
@@ -1,10 +1,18 @@
 class Welcome
 {
-	
+		//global variables
+		long contact=949494949494l;
 	public static void main(String args[])
 	{
-		
+		//local variables
+		byte age=223;
+		float marks=22.4324324233433f;
+		double salary=22.4324324233433;
+		char  gender='M';
+		boolean result=true;
 		System.out.println("welcome to java world");
+		System.out.println(marks+" : "+salary);
+		System.out.println(gender+" : "+age);
 	}
 	
 }
