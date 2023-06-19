cfea32e4647311c8ac2d8ee728ec2be3d682453b
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-06-19 12:04:48 +0530

    Day-1 Session-1

---
+++ Welcome.java
@@ -0,0 +1,18 @@
+class Welcome
+{
+	
+	public static void main(String args[])
+	{
+		
+		System.out.println("welcome to java world");
+	}
+	
+}
+//user understandable language --->JDK-->JRE-->JVM
+//machine language--0's 1's /byte code /binary code 
+
+//translator-->transalation
+//compiler--->compailation
+//.java --->.class/soruce code -->byte code...
+//javac filename.java
+//java classname -->.class -->JVM
