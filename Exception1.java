2c4714ef4daee3fd81a6f92d04dcb72f2902bd2f
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-06-23 11:59:29 +0530

    Exception Handling & Notes  Day-5

--- Exception1.java
+++ Exception1.java
@@ -2,24 +2,22 @@ package com.kpmg.exceptions;
 
 import java.util.Scanner;
 
-public class Exception1 {
-
+class Exception1 {
 	public static void main(String[] args) {
 
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter first number for division:");
-		int a = scan.nextInt();
-		System.out.println("Enter second number for division:");
-		int b = scan.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter first number division :");
+		int a = scanner.nextInt();
+		System.out.println("Enter second number division :");
+		int b = scanner.nextInt();
 		try {
-			int result = a / b;
-			System.out.println("division of two numbers  :" + result);
-		} catch (ArithmeticException e) {
-			System.out.println("Denominator cannot be zero");
+			System.out.println("division two numbers : " + (a / b));
+		} catch (ArithmeticException exception) {
+			System.out.println("denominator cannot be zero");
 		}
-		System.out.println("Remaining 1000 lines code ");
-		scan.close();
+		System.out.println("remaining  100 lines code ");
+
+		scanner.close();
 
 	}
-
 }
